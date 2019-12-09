668fa4c72ee168870227c537bbb4e21cd688fc65
daddyawesome <sablay296@gmail.com>
2019-12-09 16:49:10 +0000

    finalizing app

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
 from flask import Flask
-
-# myapp.py
-from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect
+from flask import url_for
+import dataset
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+# Connnect to database
+db = dataset.connect('sqlite:///file.db')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# create your guests table
+table = db['guests']
+
+# when someone sends a GET to / render sign_form.html
+@app.route('/', methods=['GET'])
+def sign_form():
+    return render_template('sign_form.html')
+
+# when someone sends a GET to /guest_book render guest_book.html
+@app.route('/guest_book', methods=['GET'])
+def guest_book():
+    signatures = table.find()
+    return render_template('guest_book.html', signatures=signatures)
+
+# when someone sends  POST to /submit, take the name and message from the body
+# of the POST, store it in the database, and redirect them to the guest_book
+@app.route('/submit', methods=['POST'])
+def submit():
+    signature = dict(name=request.form['name'], message=request.form['message'])
+    table.insert(signature)
+    return redirect(url_for('guest_book'))
+
+app.run(debug=True)
